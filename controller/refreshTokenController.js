3eebcb659c3022b1acd4ff87ed7c86a23d6c0e30
LeonardAzah <isongleonard@gmail.com>
2023-06-14 04:54:28 +0100

    voting refreshtoekn

--- controller/refreshTokenController.js
+++ controller/refreshTokenController.js
@@ -5,23 +5,23 @@ const Admin = db.admin;
 const Student = db.student;
 
 const userRefreshToken = async (req, res) => {
-  const cookie = req.cookie;
+  const cookie = req.cookies;
 
   if (!cookie?.jwt) return res.sendStatus(401);
 
   const refreshToken = cookie.jwt;
-  console.log(cookie);
 
   const foundUser = await Admin.findOne({
     where: { refreshtoken: refreshToken },
   });
-  console.log(foundUser);
 
   if (!foundUser) return res.sendStatus(403);
 
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
     if (err || foundUser.username != decoded.username)
       return res.sendStatus(403);
+
+    const username = foundUser.username;
 
     const accessToken = jwt.sign(
       {
